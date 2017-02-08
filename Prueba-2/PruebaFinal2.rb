2cd42703723f0eca7d4238b3e5bd6afd5f1f7ba9
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-08 12:12:34 -0300

    Otro commit

--- Prueba-2/PruebaFinal2.rb
+++ Prueba-2/PruebaFinal2.rb
@@ -1,3 +1,5 @@
+#PENDIENTE CON LA 3 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
 class T
 	def method1()
 		puts "Item 1 -Método de instancia-: metodo1. Se agrega new: T.new.method1"
@@ -12,3 +14,44 @@ class T
 	end
 end
 T.method1
+
+
+module PruebaMayor
+	class Prueba
+		@@A = 5
+		def self.A
+			@@A
+		end
+	end
+end
+prueba = PruebaMayor::Prueba.new
+puts "Item 3: #{prueba}" 
+
+
+class Complejo
+	attr_accessor :x, :y
+	def initialize(x, y)
+		@x = x
+		@y = y
+	end
+
+	def +(complejo)
+		puts Complejo.new(@x + complejo.x, @y + complejo.y)
+	end
+end
+
+
+class T
+	def method1
+		rand(0..20)
+	end
+end
+
+class Q < T
+	def initialize()
+		dev = T.new.method1
+	end
+end
+
+claseQ = Q.new.dev
+puts "Item 5: #{claseQ}"
